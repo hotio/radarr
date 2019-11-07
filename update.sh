8eea4f666e1f89637a55f6826e4769ae6fa258f0
hotio <hotio@protonmail.com>
2019-11-07 11:58:19 +0100

    add update script

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###################
+version=$(curl -fsSL "https://radarr.lidarr.audio/v1/update/aphrodite/changes?os=linux" | jq -r .[0].version)
+app=RADARR
+###################
+
+location=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "${location}" || exit 1
+
+find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG ${app}_VERSION=.*$/ARG ${app}_VERSION=${version}/g" {} \;
+
+if [[ -n "$(git status --untracked-files=no --porcelain)" ]]; then
+    git add ./*.Dockerfile
+    git commit -m "Updated version to: ${version}"
+    echo "Updated version to: ${version}"
+else
+    echo "Version is still: ${version}"
+fi
