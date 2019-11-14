55b6d428aa0c87f25d827827ebb2ec987d201d7e
hotio <hotio@protonmail.com>
2019-11-14 13:23:24 +0100

    update

--- update.sh
+++ update.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-###################
 version=$(curl -fsSL "https://api.github.com/repos/radarr/radarr/releases" | jq -r .[0].tag_name | sed s/v//g)
-app=RADARR
-###################
-
-location=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-cd "${location}" || exit 1
-
-find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG ${app}_VERSION=.*$/ARG ${app}_VERSION=${version}/g" {} \;
+find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RADARR_VERSION=.*$/ARG RADARR_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
-
-if [[ -n "$(git status --untracked-files=no --porcelain)" ]]; then
-    git add ./*.Dockerfile
-    git add ./.drone.yml
-    git commit -m "Updated version to: ${version}"
-    echo "Updated version to: ${version}"
-else
-    echo "Version is still: ${version}"
-fi
+echo "##[set-output name=version;]${version}"
