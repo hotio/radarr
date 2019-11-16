c409f04a5659e0608b9027d674c009155d66f21e
hotio <hotio@protonmail.com>
2019-11-16 09:59:43 +0100

    [CI SKIP] add failsafe

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$(curl -fsSL "https://api.github.com/repos/radarr/radarr/releases" | jq -r .[0].tag_name | sed s/v//g)
+[[ -z ${version} ]] && exit
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RADARR_VERSION=.*$/ARG RADARR_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 echo "##[set-output name=version;]${version}"
