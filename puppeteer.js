d7668ad11499ab279a8efab1e00e0d0389826d79
hotio <hotio@protonmail.com>
2019-12-13 22:49:54 +0100

    use networkidle2

--- puppeteer.js
+++ puppeteer.js
@@ -14,7 +14,7 @@ const puppeteer = require("puppeteer");
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
-  await page.goto("http://service:7878/system/status", { waitUntil: "networkidle0" });
+  await page.goto("http://service:7878/system/status", { waitUntil: "networkidle2" });
   await page.screenshot({ path: "/tmp/screenshot.png", fullPage: true });
   await browser.close();
 })();
