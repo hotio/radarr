7356efc555bccfcca05b826935b38745f00817c9
hotio <26902309+hotio@users.noreply.github.com>
2020-04-26 16:27:49 +0200

    update url [CI SKIP]

--- update.sh
+++ update.sh
@@ -42,9 +42,9 @@ elif [[ ${1} == "checkservice" ]]; then
     curl -fsSL ${SERVICE} > /dev/null
 else
     branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=aphrodite" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
-    version=$(curl -fsSL "https://radarr.lidarr.audio/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
+    version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
-    [[ ${version} == null ]] && exit 0
+    [[ ${version} == "null" ]] && exit 0
     sed -i "s/{RADARR_VERSION=[^}]*}/{RADARR_VERSION=${version}}/g" .drone.yml
     sed -i "s/{RADARR_BRANCH=[^}]*}/{RADARR_BRANCH=${branch}}/g" .drone.yml
     echo "##[set-output name=version;]${branch}-${version}"
