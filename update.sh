982b214123da443347629791d628a887d3deeb00
hotio <26902309+hotio@users.noreply.github.com>
2020-08-07 21:57:25 +0200

    switch to gha

--- update.sh
+++ update.sh
@@ -1,51 +1,9 @@
 #!/bin/bash
 
-if [[ ${1} == "screenshot" ]]; then
-    SERVICE_IP="http://$(ping -c1 -4 service | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'):7878/system/status"
-    NETWORK_IDLE="2"
-    cd /usr/src/app && node <<EOF
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch({
-    bindAddress: "0.0.0.0",
-    args: [
-      "--no-sandbox",
-      "--headless",
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-      "--remote-debugging-port=9222",
-      "--remote-debugging-address=0.0.0.0"
-    ]
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  try {
-    await page.goto("${SERVICE_IP}", { waitUntil: "networkidle${NETWORK_IDLE}" });
-  } catch (e) {
-    console.log(e)
-    process.exit(1)
-  }
-  await page.evaluate(() => {
-    const div = document.createElement('div');
-    div.innerHTML = 'Image: ${DRONE_REPO_OWNER}/${DRONE_REPO_NAME##docker-}:${DRONE_COMMIT_BRANCH}${PR_BRANCH}<br>App Version: ${VERSION_FIELD}<br>Commit: ${DRONE_COMMIT_SHA:0:7}<br>Build: #${DRONE_BUILD_NUMBER}<br>Timestamp: $(date -u --iso-8601=seconds)';
-    div.style.cssText = "all: initial !important; border-radius: 4px !important; font-weight: normal !important; font-size: normal !important; font-family: monospace !important; padding: 10px !important; color: black !important; position: fixed !important; bottom: 10px !important; right: 10px !important; background-color: #e7f3fe !important; border-left: 6px solid #2196F3 !important; z-index: 10000 !important";
-    document.body.appendChild(div);
-  });
-  await page.screenshot({ path: "/drone/src/screenshot.png", fullPage: true });
-  await browser.close();
-})();
-EOF
-elif [[ ${1} == "checkservice" ]]; then
-    SERVICE="http://service:7878"
-    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL ${SERVICE} > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
-    curl -fsSL ${SERVICE} > /dev/null
-else
-    branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=aphrodite" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
-    version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
-    [[ -z ${version} ]] && exit 1
-    [[ ${version} == "null" ]] && exit 0
-    sed -i "s/{RADARR_VERSION=[^}]*}/{RADARR_VERSION=${version}}/g" .drone.yml
-    sed -i "s/{RADARR_BRANCH=[^}]*}/{RADARR_BRANCH=${branch}}/g" .drone.yml
-    echo "##[set-output name=version;]${branch}-${version}"
-fi
+branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=aphrodite" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
+version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
+[[ -z ${version} ]] && exit 1
+[[ ${version} == "null" ]] && exit 0
+sed -i "s/{RADARR_VERSION=[^}]*}/{RADARR_VERSION=${version}}/g" .github/workflows/build.yml
+sed -i "s/{RADARR_BRANCH=[^}]*}/{RADARR_BRANCH=${branch}}/g" .github/workflows/build.yml
+echo "##[set-output name=version;]${branch}-${version}"
