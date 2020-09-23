3286c17fbc07c43696d87ff04907fafa4a22fee7
hotio <26902309+hotio@users.noreply.github.com>
2020-09-23 16:07:32 +0200

    fix version check [skip ci]

--- update.sh
+++ update.sh
@@ -24,7 +24,7 @@ else
     version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
     [[ ${version} == "null" ]] && exit 0
-    version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[].name)
+    version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
     [[ -z ${version_arr_discord_notifier} ]] && exit 1
     echo "VERSION=${version}" > VERSION
     echo "BRANCH=${branch}" >> VERSION
