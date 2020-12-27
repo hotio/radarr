9c92574b7618fbd9c1ecd771d1e54bfba48fd61e
mrhotio <26902309+mrhotio@users.noreply.github.com>
2020-12-27 19:38:15 +0100

    skip ci

--- update.sh
+++ update.sh
@@ -23,11 +23,11 @@ elif [[ ${1} == "screenshot" ]]; then
     exit 0
 else
     branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=develop" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
+    [[ -z ${branch} ]] && exit 0
     version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
     [[ ${version} == "null" ]] && exit 0
     version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
     [[ -z ${version_arr_discord_notifier} ]] && exit 1
     echo '{"version":"'"${version}"'","branch":"'"${branch}"'","arr_discord_notifier_version":"'"${version_arr_discord_notifier}"'"}' | jq . > VERSION.json
-    echo "##[set-output name=version;]${branch}-${version}/${version_arr_discord_notifier}"
 fi
