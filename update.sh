1042af798e0c9d8797f9491b4a158f17e4b541e0
mrhotio <26902309+mrhotio@users.noreply.github.com>
2021-02-13 08:33:20 +0100

    skip ci

--- update.sh
+++ update.sh
@@ -24,7 +24,7 @@ elif [[ ${1} == "screenshot" ]]; then
 else
     branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=develop" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
     [[ -z ${branch} ]] && exit 0
-    version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
+    version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux&runtime=netcore&arch=x64" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
     [[ ${version} == "null" ]] && exit 0
     version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
