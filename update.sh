20a5aa593c16d4f02ff5eb14b51b8d1a52961aa7
mrhotio <26902309+mrhotio@users.noreply.github.com>
2021-10-01 19:39:53 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -33,5 +33,4 @@ else
     version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
     [[ -z ${version_arr_discord_notifier} ]] && exit 1
     echo '{"version":"'"${version}"'","arr_discord_notifier_version":"'"${version_arr_discord_notifier}"'"}' | jq . > VERSION.json
-    echo "##[set-output name=version;]${version}/${version_arr_discord_notifier}"
 fi
