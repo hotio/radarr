7b4186f8758439fbfc615fd784d01f3c1c4ce08b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2021-10-01 19:57:05 +0200

    switch to musl

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -1,25 +1,18 @@
-FROM ghcr.io/hotio/base:focal
-
-ARG DEBIAN_FRONTEND="noninteractive"
+FROM ghcr.io/hotio/base:alpine
 
 EXPOSE 7878
 
-# install packages
-RUN apt update && \
-    apt install -y --no-install-recommends --no-install-suggests \
-        libmediainfo0v5 libicu66 && \
-# clean up
-    apt autoremove -y && \
-    apt clean && \
-    rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/*
+RUN apk add --no-cache libintl sqlite-libs icu-libs && \
+    apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/main tinyxml2 && \
+    apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community libmediainfo
 
 ARG VERSION
 ARG BRANCH
 ARG PACKAGE_VERSION={VERSION}
 RUN mkdir "${APP_DIR}/bin" && \
-    curl -fsSL "https://radarr.servarr.com/v1/update/${BRANCH}/updatefile?version=${VERSION}&os=linux&runtime=netcore&arch=arm64" | tar xzf - -C "${APP_DIR}/bin" --strip-components=1 && \
+    curl -fsSL "https://radarr.servarr.com/v1/update/${BRANCH}/updatefile?version=${VERSION}&os=linuxmusl&runtime=netcore&arch=arm64" | tar xzf - -C "${APP_DIR}/bin" --strip-components=1 && \
     rm -rf "${APP_DIR}/bin/Radarr.Update" && \
-    echo "PackageVersion=${PACKAGE_VERSION}\nPackageAuthor=[hotio](https://github.com/hotio)\nUpdateMethod=Docker\nBranch=${BRANCH}" > "${APP_DIR}/package_info" && \
+    echo -e "PackageVersion=${PACKAGE_VERSION}\nPackageAuthor=[hotio](https://github.com/hotio)\nUpdateMethod=Docker\nBranch=${BRANCH}" > "${APP_DIR}/package_info" && \
     chmod -R u=rwX,go=rX "${APP_DIR}"
 
 ARG ARR_DISCORD_NOTIFIER_VERSION
--- update.sh
+++ update.sh
@@ -4,14 +4,13 @@ if [[ ${1} == "checkdigests" ]]; then
     exit 0
 elif [[ ${1} == "tests" ]]; then
     echo "List installed packages..."
-    docker run --rm --entrypoint="" "${2}" apt list --installed
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
     echo "Check if app works..."
     app_url="http://localhost:7878/system/status"
     docker run --network host -d --name service "${2}"
     currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
     curl -fsSL "${app_url}" > /dev/null
     status=$?
-    [[ ${2} == *"linux-arm-v7" ]] && status=0
     echo "Show docker logs..."
     docker logs service
     exit ${status}
@@ -24,7 +23,7 @@ elif [[ ${1} == "screenshot" ]]; then
 else
     branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=develop" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
     [[ -z ${branch} ]] && exit 0
-    version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linux&runtime=netcore&arch=x64" | jq -r .[0].version)
+    version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
     [[ ${version} == "null" ]] && exit 0
     version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
