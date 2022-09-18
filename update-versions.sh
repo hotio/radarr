c9dcd481b3cee3cc116c3514f37c16e35398e424
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-18 07:31:27 +0200

    fix

--- update-versions.sh
+++ update-versions.sh
@@ -7,4 +7,4 @@ version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=
 version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
 [[ -z ${version_arr_discord_notifier} ]] && exit 0
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .arr_discord_notifier_version = "'"${version_arr_discord_notifier}"'" | .branch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'" | .arr_discord_notifier_version = "'"${version_arr_discord_notifier}"'" | .sbranch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
