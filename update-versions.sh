20dfe497616d334b010717f86bd2cf43d1ef0c03
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-01-04 22:49:57 +0100

    Remove Arr Discord Notifier [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -4,7 +4,5 @@ branch="nightly"
 version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
 [[ -z ${version} ]] && exit 0
 [[ ${version} == "null" ]] && exit 0
-version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
-[[ -z ${version_arr_discord_notifier} ]] && exit 0
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .arr_discord_notifier_version = "'"${version_arr_discord_notifier}"'" | .sbranch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'" | .sbranch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
