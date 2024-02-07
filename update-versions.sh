0df6314215c7d55440b8585874277c0525dfab78
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 10:42:29 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,7 +2,7 @@
 set -e
 branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=develop" | jq -re 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref')
 branch=$(tail -n 1 <<< "${branch}")
-version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re .[0].version)
+version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version')
 curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/updatefile?version=${version}&os=linuxmusl&runtime=netcore&arch=x64" -o /dev/null || exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
