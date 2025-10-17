c61f36a6caaabe206b55f8ff6c5d88884cb520b9
mrhotio <26902309+mrhotio@users.noreply.github.com>
2025-10-17 08:10:10 +0200

    tidy up version check [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=develop" | jq -re 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref') || exit 1
-branch=$(tail -n 1 <<< "${branch}")
+branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&base=develop&sort=updated&direction=desc" | jq -re '[sort_by(.updated_at) | .[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not)) | .head.ref][0]') || exit 1
 version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version') || exit 1
 curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/updatefile?version=${version}&os=linuxmusl&runtime=netcore&arch=x64" -o /dev/null || exit 1
 json=$(cat VERSION.json)
