913d54108bf46e4bf1be66e9ad3850835ffa3fc9
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 09:08:21 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -exuo pipefail
+
 sbranch="develop"
-version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${sbranch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version') || exit 1
+version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${sbranch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version')
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
