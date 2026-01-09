512e849e78134265eb0719fbbb875e4a97769c9b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 09:06:38 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&sort=updated&direction=desc" | jq -re '[.[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not) and (.base.ref == "develop")) | .head.ref][0]') || exit 1
-version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version') || exit 1
-curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/updatefile?version=${version}&os=linuxmusl&runtime=netcore&arch=x64" -o /dev/null || exit 1
+set -exuo pipefail
+
+branch=$(curl -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&sort=updated&direction=desc" | jq -re '[.[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not) and (.base.ref == "develop")) | .head.ref][0]')
+version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version')
+curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/updatefile?version=${version}&os=linuxmusl&runtime=netcore&arch=x64" -o /dev/null
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
