1327cac85a00c079ebe2ac54408c150a647a69a6
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-13 10:08:41 +0100

    rename args [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -exuo pipefail
 
-branch=$(curl -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&sort=updated&direction=desc" | jq -re '[.[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not) and (.base.ref == "develop")) | .head.ref][0]')
-version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version')
-curl -fsSL "https://radarr.servarr.com/v1/update/${branch}/updatefile?version=${version}&os=linuxmusl&runtime=netcore&arch=x64" -o /dev/null
+version_branch=$(curl -fsSL "https://api.github.com/repos/radarr/radarr/pulls?state=open&sort=updated&direction=desc" | jq -re '[.[] | select((.head.repo.full_name == "Radarr/Radarr") and (.head.ref | contains("dependabot") | not) and (.base.ref == "develop")) | .head.ref][0]')
+version=$(curl -fsSL "https://radarr.servarr.com/v1/update/${version_branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version')
+curl -fsSL "https://radarr.servarr.com/v1/update/${version_branch}/updatefile?version=${version}&os=linuxmusl&runtime=netcore&arch=x64" -o /dev/null
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg branch "${branch}" \
-    '.version = $version | .branch = $branch' <<< "${json}" | tee meta.json
+    --arg version_branch "${version_branch}" \
+    '.version = $version | .version_branch = $version_branch' <<< "${json}" | tee meta.json
